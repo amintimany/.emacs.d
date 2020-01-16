2eacf9f32b79ccfd2b98af74efa08ee2e8ffe70c
Amin Timany <amintimany@gmail.com>
2020-01-16 09:53:02 +0100

    use fzf

--- config.el
+++ config.el
@@ -147,6 +147,7 @@
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-x l") 'counsel-fzf)
 (global-set-key (kbd "C-h f") 'counsel-describe-function)
 (global-set-key (kbd "C-h v") 'counsel-describe-variable)
 (global-set-key (kbd "C-h l") 'counsel-find-library)

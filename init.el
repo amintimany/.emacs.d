d4915a9ef173c9382431752be457fa40e85b0eeb
Amin Timany <amintimany@gmail.com>
2023-11-29 18:48:20 +0100

    update proof general and tuareg

--- init.el
+++ init.el
@@ -6,12 +6,10 @@
  '(ansi-color-names-vector
    ["#3c3836" "#fb4933" "#b8bb26" "#fabd2f" "#83a598" "#d3869b" "#8ec07c" "#ebdbb2"])
  '(custom-safe-themes
-   (quote
-    ("947190b4f17f78c39b0ab1ea95b1e6097cc9202d55c73a702395fc817f899393" "8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a" "a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" "585942bb24cab2d4b2f74977ac3ba6ddbd888e3776b9d2f993c5704aa8bb4739" "1436d643b98844555d56c59c74004eb158dc85fc55d2e7205f8d9b8c860e177f" "bd7b7c5df1174796deefce5debc2d976b264585d51852c962362be83932873d9" default)))
+   '("947190b4f17f78c39b0ab1ea95b1e6097cc9202d55c73a702395fc817f899393" "8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a" "a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" "585942bb24cab2d4b2f74977ac3ba6ddbd888e3776b9d2f993c5704aa8bb4739" "1436d643b98844555d56c59c74004eb158dc85fc55d2e7205f8d9b8c860e177f" "bd7b7c5df1174796deefce5debc2d976b264585d51852c962362be83932873d9" default))
  '(package-selected-packages
-   (quote
-    (counsel ivy dracula-theme sml-modeline ocp-indent multiple-cursors monokai-theme markdown-mode magit git-gutter company-coq column-enforce-mode auctex)))
- '(pdf-view-midnight-colors (quote ("#fdf4c1" . "#1d2021"))))
+   '(company counsel ivy dracula-theme sml-modeline ocp-indent multiple-cursors monokai-theme markdown-mode magit git-gutter company-coq column-enforce-mode auctex))
+ '(pdf-view-midnight-colors '("#fdf4c1" . "#1d2021")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
